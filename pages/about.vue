920863184f96099909dead409dab9b8f4268429e
liuti <Tianqing.Liu@moodys.com>
2020-04-23 15:09:02 +0200

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        persoHomePage
+      </h1>
+      <h2 class="subtitle">
+        github transfer
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
